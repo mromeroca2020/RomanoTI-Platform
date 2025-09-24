6ded76b6017bcfa78a3712ed75391755c197a7f7
mromeroca2020 <58543051+mromeroca2020@users.noreply.github.com>
2025-09-23 20:29:34 -0400

    Update AuthResponse.java

--- backend/src/main/java/com/romanoti/dto/AuthResponse.java
+++ backend/src/main/java/com/romanoti/dto/AuthResponse.java
@@ -1,1 +1,106 @@
+package com.romanoti.dto;
 
+import java.util.UUID;
+
+public class AuthResponse {
+    private String token;
+    private String type = "Bearer";
+    private UUID id;
+    private String email;
+    private String role;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+
+    // Constructores
+    public AuthResponse() {}
+
+    public AuthResponse(String token, UUID id, String email, String role, String firstName, String lastName) {
+        this.token = token;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + lastName;
+    }
+
+    // Getters y Setters
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthResponse{" +
+                "token='[PROTECTED]'" +
+                ", type='" + type + '\'' +
+                ", id=" + id +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+}
